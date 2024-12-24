45fc001c2807c22ec59c1368ae4ba1cdee05952f
unknown <joris.blain@lasocietenouvelle.org>
2024-12-24 17:39:08 +0100

    refactoring

--- 01_aggregate_value_added_files.R
+++ 01_aggregate_value_added_files.R
@@ -1,39 +1,52 @@
-library(tidyverse)
+x = c('tidyverse','data.table','arrow')
+lapply(x, library,character.only = T)
 
-setwd("C:/Users/JAX/Desktop/sektor_analyse")
-
-# Pfad zum Ordner, in dem sich die Dateien befinden
-path <- "./data/values"
-
-# Liste der Dateien im Ordner, die mit einer Zahl beginnen
-filenames <- list.files(path, pattern = "matrix_eu-ic-io_ind-by-ind_23ed", full.names = TRUE)
+#ref_area refers to rows
+#counterpart refers to columns
 
 read_csv_with_time <- function(file_path) {
   # Read the CSV file
-  df <- read_csv(file_path, show_col_types = FALSE)
-  
-  # Extract time period from filename
-  time_period <- gsub(".*_(\\d{4})\\.csv", "\\1", file_path)
-  
-  # Add 'time_period' column
-  df$time_period <- time_period
-  
+  df <- fread(file_path) %>%
+    # Add 'time_period' column
+    mutate(time_period = gsub(".*_(\\d{4})\\.csv", "\\1", file_path))
+
   return(df)
 }
 
-# Read and combine CSV files while adding 'time_period' column
-df <- filenames |> 
-  map_dfr(read_csv_with_time) |> 
-  separate(rowLabels, into = c("ref_area", "industry"), sep = "_",  extra = "merge") 
 
+format_iot = function(folder,
+                      exdir,
+                      update = F,
+                      verbose = T)
+{
 
-df_long <- df |> 
-  pivot_longer(
-    cols = -c(ref_area, industry, time_period),
-    names_to = c("counterpart_area", ".value"),
-    names_pattern = "^(\\D+1?)_(\\w*_?)"  )
-# Display the resulting dataframe
-print(df_long)
+  if(!update)
+  {
+    if(file.exists(paste0(exdir,"/values_agg_23.parquet")))
+    {
+      if(verbose) print('Returning cached data')
+      return(read_parquet(paste0(exdir,"/values_agg_23.parquet")))
+    }
+    if(verbose) print('File does not exists yet, updating...')
+  }
 
-# Save as RDS file
-readr::write_rds(df_long, "data/values/values_agg_23.rds", compress="gz")
+  filenames <- list.files(folder, pattern = "matrix_eu-ic-io_ind-by-ind_23ed", full.names = TRUE)
+
+  df <- filenames |>
+    map_dfr(read_csv_with_time)
+  # %>%
+  #   separate(rowLabels, into = c("ref_area", "industry"), sep = "_",  extra = "merge") |>
+  #   pivot_longer(
+  #     cols = -c(ref_area, industry, time_period),
+  #     names_to = c("counterpart_area", ".value"),
+  #     names_pattern = "^(\\D+1?)_(\\w*_?)"  )
+
+  if(verbose) print(paste0('Data cached in ',exdir,"/values_agg_23.parquet"))
+
+  write_parquet(df, paste0(exdir,"/values_agg_23.parquet"))
+
+}
+
+format_iot(folder = "C:/Users/Joris/OneDrive - La Société Nouvelle/Partage/FIGARO ed23",
+           exdir = "C:/Users/Joris/OneDrive - La Société Nouvelle/Partage/FIGARO ed23",
+           update = T)
